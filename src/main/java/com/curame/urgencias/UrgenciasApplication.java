e3154f9c258a3a296c97133739d487517afdb419
ivansoto <lopezsotoivan01@gmail.com>
2022-05-24 19:28:58 -0500

    first commit

---
+++ src/main/java/com/curame/urgencias/UrgenciasApplication.java
@@ -0,0 +1,13 @@
+package com.curame.urgencias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrgenciasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrgenciasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/curame/urgencias/UrgenciasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.curame.urgencias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrgenciasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
